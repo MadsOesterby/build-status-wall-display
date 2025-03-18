96f412ae9a1427df3158832cb03bc1fc54bc0b3a
noaho <noaho@NOAHS-HP>
2025-03-18 14:16:13 +0100

    Add project files.

---
+++ Red and Green boxes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Red_and_Green_boxes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
