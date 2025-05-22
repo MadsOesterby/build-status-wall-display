542936d5688efd45a7b78175547d9f04e29e1e7a
Mads Østerby <mads.oesterby@3shape.com>
2025-05-22 10:14:56 +0200

    Increase page auto-refresh rate to 30 secs

--- build-status-wall-display/Controllers/HomeController.cs
+++ build-status-wall-display/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BuildStatusWallDisplay.Controllers
         private readonly IGitHubService _gitHubService;
         private readonly IMemoryCache _memoryCache;
         private const string CacheKey = "WorkflowStatuses";
-        private static readonly TimeSpan CacheExpiration = TimeSpan.FromSeconds(30);
+        private static readonly TimeSpan CacheExpiration = TimeSpan.FromSeconds(29);
 
         public HomeController(
             ILogger<HomeController> logger,
